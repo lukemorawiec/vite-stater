37ad661598aabb8df40bb4316ba25b73c27a9320
Luke Morawiec <luke.morawiec@gmail.com>
2023-06-30 20:33:47 +0200

    Introduce TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  function countCharacters(str: string): number {
+    return str.length;
+  }
+
+  function isEven(value: number): boolean {
+    return value % 2 === 0;
+  }
+
+  function getParityMessage(isEven: boolean): string {
+    if (isEven) {
+      return "Ta liczba jest parzysta.";
+    }
+    return "Ta liczba nie jest parzysta.";
+  }
+
+  function processString(str?: string): string {
+    if (!str) {
+      return "Nie podano słowa";
+    }
+    const length = countCharacters(str);
+    const even = isEven(length);
+    const message = getParityMessage(even);
+    return `${str} - Liczba znaków: ${length}. ${message}`;
+  }
+
+  const inputString = "Hello, world!";
+  const outputString = processString(inputString);
+  console.log(outputString);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Wstęp do TSa</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
