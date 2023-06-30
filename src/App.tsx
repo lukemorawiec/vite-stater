a8bb89ffc9840fc8caca762c3de357f5ed0318c0
Luke Morawiec <luke.morawiec@gmail.com>
2023-06-30 21:07:05 +0200

    Add enum

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,13 @@
 import "./App.css";
+import WordParser from "./WordParser/WordParser";
 
 function App() {
-  function countCharacters(str: string): number {
-    return str.length;
-  }
-
-  function isEven(value: number): boolean {
-    return value % 2 === 0;
-  }
-
-  function getParityMessage(isEven: boolean): string {
-    if (isEven) {
-      return "Ta liczba jest parzysta.";
-    }
-    return "Ta liczba nie jest parzysta.";
-  }
-
-  function processString(str?: string): string {
-    if (!str) {
-      return "Nie podano słowa";
-    }
-    const length = countCharacters(str);
-    const even = isEven(length);
-    const message = getParityMessage(even);
-    return `${str} - Liczba znaków: ${length}. ${message}`;
-  }
-
-  const inputString = "Hello, world!";
-  const outputString = processString(inputString);
-
   return (
     <>
       <h1>Wstęp do TSa</h1>
-      <div>{outputString}</div>
+      <div>
+        <WordParser input="Hello" prop="123" />
+      </div>
     </>
   );
 }
---
+++ src/WordParser/WordParser.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+export type Prop = {
+  prop: string;
+};
+
+export interface WordParserProps extends Prop {
+  input: string;
+}
+
+enum Status {
+  DONE = "done",
+  NEW = "new",
+  NOT_DONE = "not_done",
+}
+
+interface Todo {
+  title: string;
+  id: number;
+  content: string;
+  status: Status;
+}
+
+export default function WordParser({ input, prop }: WordParserProps) {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  function countCharacters(str: string): number {
+    return str.length;
+  }
+
+  function isEven(value: number): boolean {
+    return value % 2 === 0;
+  }
+
+  function getParityMessage(isEven: boolean): string {
+    if (isEven) {
+      return "Ta liczba jest parzysta.";
+    }
+    return "Ta liczba nie jest parzysta.";
+  }
+
+  function processString(str?: string): string {
+    if (!str) {
+      return "Nie podano słowa";
+    }
+    const length = countCharacters(str);
+    const even = isEven(length);
+    const message = getParityMessage(even);
+    return `${str} - Liczba znaków: ${length}. ${message}`;
+  }
+
+  const outputString = processString(input);
+  return (
+    <div>
+      {todos[0].status === Status.DONE && outputString}
+
+      {todos.map((todo) => {
+        if (todo.content) {
+          return todo.content;
+        }
+      })}
+    </div>
+  );
+}
